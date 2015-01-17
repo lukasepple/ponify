fa9f82486b983462110b6319718d04f2d235ca6b
lukasepple <git@lukasepple.de>
2015-01-17 11:42:21 +0100

    new approach of replacing still needs to get some improvemnts for in-word replacement

--- Main.hs
+++ Main.hs
@@ -8,25 +8,40 @@ import Options
 ponify :: T.Text -> [(T.Text, T.Text)] -> T.Text
 ponify input rules
   | null rules = input
-  | otherwise  = ponify (uncurry T.replace (head rules) input) (tail rules)
+  | otherwise  = ponify (uncurry globalReplace (head rules) input) (tail rules)
 
 deponify :: T.Text -> [(T.Text, T.Text)] -> T.Text
 deponify input rules = ponify input (map swap rules)
 
+-- rules are just full text replaces with some extra glue
+-- the markup of the rules file looks like this:
+-- search :: replace
+-- the word search gets replaced by replace
+
 parseRules :: String -> [(T.Text, T.Text)]
 parseRules = map (composeRule . words) . dropComments . lines
   where dropComments = filter (\x -> not (null x) && head x /= '#')
-        composeRule list = mapTuple (expandPattern . T.pack . unwords) (take dotsIndex list, drop (dotsIndex + 1) list)
+        composeRule list = mapTuple (T.pack . unwords) (take dotsIndex list, drop (dotsIndex + 1) list)
           where mapTuple f t = (f (fst t), f (snd t))
-                expandPattern p = let hasLeadingAsteriks = T.head p == '*'
-                                      hasFinalAsteriks   = T.last p == '*'
-                                      calcExpanded :: Bool -> T.Text
-                                      calcExpanded bool = if bool then T.empty else T.pack " " in
-                                  calcExpanded hasLeadingAsteriks `T.append` p ` T.append` calcExpanded hasFinalAsteriks
                 dotsIndex = fromJust (elemIndex "::" list)
 
+globalReplace :: T.Text -> T.Text -> T.Text -> T.Text
+globalReplace needle repl haystack = if needle `T.isInfixOf` haystack
+                                     then
+                                      T.unlines (map (T.unwords . replaceLine . T.words) (T.lines haystack))
+                                     else
+                                      haystack
+  where replaceLine line = map (\word -> replaceWord word) line
+        replaceWord word
+          | word == needle = repl
+          | needle `T.isInfixOf` word &&
+            onlyPunctuation (foldl1 T.append (T.splitOn needle word)) = T.replace needle repl word
+          | otherwise      = word
+          where onlyPunctuation str = T.foldl (\acc c -> c `elem` ".?!,\"\' " && acc) True str
+
 data MainOptions = MainOptions
-  { optPonify :: Bool,
+  {
+    optPonify :: Bool,
     optDeponify :: Bool,
     optRules :: String
   }
